28a860c93c3ae70d2a51bc6913bd27f5052d5dc5
Xeth <adamhung12@gmail.com>
2018-12-03 11:35:21 +0800

    1. update test source 2. update gradle script

--- src/test/java/me/xeth/utils/JDBCProvider/TestOne.java
+++ src/test/java/me/xeth/utils/JDBCProvider/TestOne.java
@@ -4,6 +4,7 @@ import me.xethh.utils.JDBCProvider.ImmutableJDBCProvider;
 import me.xethh.utils.JDBCProvider.JDBCProvider;
 import me.xethh.utils.JDBCProvider.JDBCProviderFactory;
 import me.xethh.utils.JDBCProvider.PersistedJDBCProvider;
+import org.junit.Test;
 import org.slf4j.LoggerFactory;
 
 import java.sql.ResultSet;
@@ -14,39 +15,57 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
+import static org.junit.Assert.*;
+
 public class TestOne {
-    public static void main(String[] args){
-        org.slf4j.Logger logger = LoggerFactory.getLogger(ImmutableJDBCProvider.class);
-        Logger log = Logger.getLogger("my.logger");
+    static org.slf4j.Logger logger = LoggerFactory.getLogger(ImmutableJDBCProvider.class);
+    static Logger log = Logger.getLogger("my.logger");
+    static {
         log.setLevel(Level.ALL);
         ConsoleHandler handler = new ConsoleHandler();
         handler.setFormatter(new SimpleFormatter());
         log.addHandler(handler);
-
-        JDBCProvider conn = JDBCProviderFactory.mysqlProvider("jdbc:mysql://localhost:3306/explorer_playgroup?useUnicode=yes&amp&characterEncoding=UTF-8", "explorer_playgroup", "explorer_playgroup");
+    }
+    @Test
+    public void testConnection(){
+        JDBCProvider conn = JDBCProviderFactory.mysqlProvider("jdbc:mysql://localhost:3306/testing?useUnicode=yes&amp&characterEncoding=UTF-8", "testing", "testing");
         try {
-            Statement stmt = conn.getConnection().createStatement();
-            ResultSet rs = stmt.executeQuery("show tables");
-            while(rs.next()){
-//                logger.info(rs.getString(1));
-            }
             logger.info("Compare one time connection");
             logger.info(conn.getConnection().toString());
             logger.info(conn.getConnection().toString());
+            Statement stmt = conn.getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery("select 1");
+            while(rs.next()){
+                assertTrue(rs.getInt(1)==1);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+    @Test
+    public void testConnectionPersisted(){
+        JDBCProvider conn = JDBCProviderFactory.mysqlProvider("jdbc:mysql://localhost:3306/testing?useUnicode=yes&amp&characterEncoding=UTF-8", "testing", "testing");
+        String conn1Str = conn.getConnection().toString();
+        String conn2Str = conn.getConnection().toString();
+        logger.info("Compare non persisted connection");
+        assertNotEquals(conn1Str,conn2Str);
+        logger.info(conn.getConnection().toString());
+        logger.info(conn.getConnection().toString());
+
         PersistedJDBCProvider conn2 = JDBCProviderFactory.mysqlPersistedProvider("jdbc:mysql://localhost:3306/explorer_playgroup?useUnicode=yes&amp&characterEncoding=UTF-8", "explorer_playgroup", "explorer_playgroup");
         try {
             logger.info("Compare persisted connection");
-            logger.info(conn2.getConnection().toString());
-            logger.info(conn2.getConnection().toString());
+            String conn2_1String = conn2.getConnection().toString();
+            String conn2_2String = conn2.getConnection().toString();
+            assertEquals(conn2_1String,conn2_2String);
             conn2.getConnection().close();
-            logger.info(conn2.getConnection().toString());
-            logger.info(conn2.getConnection().toString());
+            String conn2_3String = conn2.getConnection().toString();
+            assertNotEquals(conn2_1String,conn2_3String);
             conn2.clearConnection();
+            String conn2_4String = conn2.getConnection().toString();
+            assertNotEquals(conn2_1String,conn2_4String);
+            assertNotEquals(conn2_3String,conn2_4String);
             logger.info(conn2.getConnection().toString());
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
