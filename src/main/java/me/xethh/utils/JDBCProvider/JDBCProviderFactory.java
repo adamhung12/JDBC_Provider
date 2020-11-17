16031b7021d80b4509eddb31b70dab8d9c197003
Xeth Hung <adamhung12@gmail.com>
2020-11-17 21:18:14 +0800

    1. migrate to maven 2. add hikari connection pool

--- src/main/java/me/xethh/utils/JDBCProvider/JDBCProviderFactory.java
+++ src/main/java/me/xethh/utils/JDBCProvider/JDBCProviderFactory.java
@@ -1,5 +1,7 @@
 package me.xethh.utils.JDBCProvider;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import me.xethh.utils.JDBCProvider.impl.ImmutableJDBCProvider;
 import me.xethh.utils.JDBCProvider.impl.PersistedDBCProviderImpl;
 
@@ -12,6 +14,23 @@ public class JDBCProviderFactory {
     }
     public static JDBCProvider mysqlNewProvider(String connectionString, String username, String password){
         return createProvider(JDBCDrivers.Mysql_New.driver(),connectionString,username,password);
+    }
+    public static HikariDataSource mysqlDataSource(String connectionString, String username, String password){
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(connectionString);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.addDataSourceProperty("cachePrepStmts",true);
+        config.addDataSourceProperty("prepStmtCacheSize",512);
+        config.addDataSourceProperty("prepStmtCacheSqlLimit",true);
+        config.addDataSourceProperty("useServerPrepStmts",true);
+        config.addDataSourceProperty("useLocalSessionState",true);
+        config.addDataSourceProperty("rewriteBatchedStatements",true);
+        config.addDataSourceProperty("cacheResultSetMetadata",true);
+        config.addDataSourceProperty("cacheServerConfiguration",true);
+        config.addDataSourceProperty("elideSetAutoCommits",true);
+        config.addDataSourceProperty("maintainTimeStats",false);
+        return new HikariDataSource(config);
     }
     public static PersistedJDBCProvider createPersistedProvider(String jdbcDriver, String connectionString, String username, String password){
         return new PersistedDBCProviderImpl(jdbcDriver,connectionString,username,password);
